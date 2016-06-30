c82eb66973e7e92269b097e2e7ad3793d9ee7cc1
Gregor Boirie <gregor.boirie@free.fr>
2016-06-30 18:23:04 +0200

    enhance invensense icm20608 imu test
    Automatically find device and hardware trigger by family name. Also setup
    timestamping clock if supported.

--- skel/usr/bin/icm20608.sh
+++ skel/usr/bin/icm20608.sh
@@ -1,14 +1,35 @@
-#!/bin/sh
+#!/bin/sh -e
 
-devno=2
-trigno=0
+devname=icm20608
 
 # The base sysfs directory where IIO devices and triggers are located
 base=/sys/bus/iio/devices
+
+# Return  Find corresponding trigger number
+trig_get_hw()
+{
+	local devno=$1
+	local devname=$2
+	local trigname="devname-dev$devno"
+	local trigno
+
+	trigno=$(lsiio | sed -n \
+	         "/^Trigger.*$trigname/s/Trigger \([0-9]\+\): $trigname/\1/p")
+	echo $((trigno))
+}
+
+# Find IIO device number
+devfilt="/^Device.*$devname/s/Device \([0-9]\+\): $devname/\1/ p"
+devno=$(($(lsiio | sed -n "$devfilt")))
 dev=$base/iio:device$devno
 
 # Disable buffered sampling first to allow sampling setup
 echo 0 > $dev/buffer/enable
+
+# If available, select the system monotonic timestamping clock for dates
+if test -f $dev/current_timestamp_clock; then
+	echo monotonic > $dev/current_timestamp_clock
+fi
 
 # Setup sampling frequency to 500 Hz
 echo 500 > $dev/sampling_frequency
@@ -20,4 +41,5 @@ echo 500 > $dev/sampling_frequency
 # -T <IIO trigger id of barometer device>
 # -l <kernel to user buffer depth in # of complete samples>
 # -c <# of sampling loops before exit>
-iio_generic_buffer -a -N $devno -T $trigno -l 2 -c -1
+iio_generic_buffer -a -N $devno -T $(trige_get_hw "$devno" "$devname") -l 2 \
+                   -c -1
