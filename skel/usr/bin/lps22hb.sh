a5affa3c41a375ce74b6d5d42b08630cc1337248
Gregor Boirie <gregor.boirie@free.fr>
2016-06-30 18:29:55 +0200

    enhance stmicro lps22hb barometer test
    Automatically find device by family name. Instantiate hrtimer trigger
    automatically (no interrupt supported for now). Also setup timestamping
    clock if supported.

--- skel/usr/bin/lps22hb.sh
+++ skel/usr/bin/lps22hb.sh
@@ -1,28 +1,47 @@
 #!/bin/sh
 
-devno=0
-trigno=0
+devname=lps22hb
 
 # The base sysfs directory where IIO devices and triggers are located
 base=/sys/bus/iio/devices
+
+# Instantiate hrtimer trigger and return its index in the global trigger space
+trig_create_hrtimer()
+{
+	local devno=$1
+	local hz=$2
+	local trigname="hrtimer-dev$devno"
+	local trigno
+
+	mkdir -p /config/iio/triggers/hrtimer/$trigname
+
+	trigno=$(lsiio | sed -n \
+	         "/^Trigger.*$trigname/s/Trigger \([0-9]\+\): $trigname/\1/p")
+	trigno=$((trigno))
+
+	echo $hz > $base/trigger$trigno/sampling_frequency
+
+	echo $trigno
+}
+
+# Find IIO device
+devfilt="/^Device.*$devname/s/Device \([0-9]\+\): $devname/\1/ p"
+devno=$(($(lsiio | sed -n "$devfilt")))
 dev=$base/iio:device$devno
 
 # Disable buffered sampling first to allow sampling setup
 echo 0 > $dev/buffer/enable
 
-# Enable sampling channels: a complete sample will contain pressure, temperature
-# and date.
-echo 1 > $dev/scan_elements/in_pressure_en
-echo 1 > $dev/scan_elements/in_temp_en
-echo 1 > $dev/scan_elements/in_timestamp_en
+# If available, select the system monotonic timestamping clock for dates
+if test -f $dev/current_timestamp_clock; then
+	echo monotonic > $dev/current_timestamp_clock
+fi
 
-# Setup sampling frequency to 75 Hz
-echo 75 > $dev/sampling_frequency
-
-# Run an everlasting sampling process using the default device's hardware
-# trigger
+# Run an everlasting sampling process using the above trigger.
+# -a enable all channels
 # -N <IIO device id of barometer device>
 # -T <IIO trigger id of barometer device>
 # -l <kernel to user buffer depth in # of complete samples>
 # -c <# of sampling loops before exit>
-iio_generic_buffer -N devno -T trigno -l 2 -c -1
+iio_generic_buffer -a -N $devno -T $(trig_create_hrtimer "$devno" "75") -l 2 \
+                   -c -1
